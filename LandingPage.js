d4d11cdcb7711ab4f565b3edd3da68f417410e27
Christian Arroyave <christianarroyave@yahoo.com>
2023-12-05 21:05:18 -0500

    commit

---
+++ LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h2>Welcome to the Photoshop App!</h2>
+      <p>Click the link below to access the app.</p>
+      <a href="/app">Go to App</a>
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Slider from './Slider'
 import SidebarItem from './SidebarItem'
 
+
+
 const DEFAULT_OPTIONS = [
   {
     name: 'Brightness',
@@ -73,56 +75,106 @@ const DEFAULT_OPTIONS = [
       max: 20
     },
     unit: 'px'
-  }
+  },
+  {
+    name: 'Color Inversion',
+    property: 'invert',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Transparency',
+    property: 'opacity',
+    value: 100,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
 ]
 
 function App() {
-  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
-  const [options, setOptions] = useState(DEFAULT_OPTIONS)
-  const selectedOption = options[selectedOptionIndex]
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  const [history, setHistory] = useState([]);
+  const [historyIndex, setHistoryIndex] = useState(-1);
 
   function handleSliderChange({ target }) {
-    setOptions(prevOptions => {
-      return prevOptions.map((option, index) => {
-        if (index !== selectedOptionIndex) return option
-        return { ...option, value: target.value }
-      })
-    })
+    const newOptions = options.map((option, index) => {
+      return index === selectedOptionIndex ? { ...option, value: target.value } : option;
+    });
+
+    setOptions(newOptions);
+
+    setHistory((prevHistory) => [...prevHistory.slice(0, historyIndex + 1), newOptions]);
+    setHistoryIndex(historyIndex + 1);
   }
 
   function getImageStyle() {
-    const filters = options.map(option => {
-      return `${option.property}(${option.value}${option.unit})`
-    })
+    const filters = options.map((option) => {
+      return `${option.property}(${option.value}${option.unit})`;
+    });
 
-    return { filter: filters.join(' ') }
+    return { filter: filters.join(' ') };
   }
 
-  console.log(getImageStyle())
+  function handleUndo() {
+    if (historyIndex > 0) {
+      setHistoryIndex(historyIndex - 1);
+      setOptions(history[historyIndex - 1]);
+    }
+  }
+
+  function handleRedo() {
+    if (historyIndex < history.length - 1) {
+      setHistoryIndex(historyIndex + 1);
+      setOptions(history[historyIndex + 1]);
+    }
+  }
+  
+  function handleReset() {
+    setOptions(DEFAULT_OPTIONS);
+    setHistory([]);
+    setHistoryIndex(-1);
+  }
 
   return (
     <div className="container">
       <div className="main-image" style={getImageStyle()} />
       <div className="sidebar">
-        {options.map((option, index) => {
-          return (
-            <SidebarItem
-              key={index}
-              name={option.name}
-              active={index === selectedOptionIndex}
-              handleClick={() => setSelectedOptionIndex(index)}
-            />
-          )
-        })}
+        {options.map((option, index) => (
+          <SidebarItem
+            key={index}
+            name={option.name}
+            active={index === selectedOptionIndex}
+            handleClick={() => setSelectedOptionIndex(index)}
+          />
+        ))}
+        <div className="buttons">
+          <button className="sidebar-button" onClick={handleUndo} disabled={historyIndex <= 0}>
+            Undo
+          </button>
+          <button className="sidebar-button" onClick={handleRedo} disabled={historyIndex >= history.length - 1}>
+            Redo
+          </button>
+          <button className="sidebar-button" onClick={handleReset} disabled={historyIndex === -1}>
+            Reset
+          </button>
+        </div>
       </div>
       <Slider
-        min={selectedOption.range.min}
-        max={selectedOption.range.max}
-        value={selectedOption.value}
+        min={options[selectedOptionIndex].range.min}
+        max={options[selectedOptionIndex].range.max}
+        value={options[selectedOptionIndex].value}
         handleChange={handleSliderChange}
       />
     </div>
-  )
+  );
 }
 
 export default App;
